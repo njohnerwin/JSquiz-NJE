bf23b1511ec5d739d54efbc3e0541284a38bbe16
njohnerwin <njohnerwin@gmail.com>
2020-05-15 17:17:35 -0400

    Added a basic outline for the JS functions and question objects.

---
+++ script.js
@@ -0,0 +1,60 @@
+//Variable declaration
+
+var timer = 200;
+
+
+//The Questions!
+var questionTest = {
+    id: 0,
+    question: "This question is a placeholder, we're only using it for testing. Which type of bird is worth the most?",
+    answer0: "Two in the bush",
+    answer1: "One in the hand",
+    answer2: "Three in Brazil somewhere",
+    answer3: "Four in a documentary you saw the other day on the Discovery Channel",
+    correct: 1 
+};
+
+console.log(questionTest);
+console.log(questionTest.id);
+console.log(questionTest.question);
+console.log(questionTest.answer0);
+console.log(questionTest.answer1);
+console.log(questionTest.answer2);
+console.log(questionTest.answer3);
+
+//Functions
+
+//Saves the user's score at the end of the game
+function saveScore() {
+
+}
+
+//Prints the High Score list
+function printScores() {
+
+}
+
+//Randomizes and lines up the next question
+function randomizeQuestion() {
+
+}
+
+//Prints the selected question
+function printQuestion() {
+
+}
+
+//Initializes the timer at the start of the game
+function setTimer() {
+
+}
+
+//Subtracts time from the timer when the user answers incorrectly
+function subtractTimer () {
+
+}
+
+//Checks if the user's answer was correct or incorrect
+function verifyAnswer () {
+
+}
